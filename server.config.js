aeccf9cbcb1265b0db8807e42fb1e60aad1f4d54
dogzy123 <dogzy123@mail.ru>
2018-11-16 21:57:06 +0200

    server config

---
+++ server.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    url     : 'http://localhost/',
+    port    : 8080
+};
--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const path      = require('path');
 const express   = require('express');
 const server    = express();
-const port      = 8080;
+const config    = require('./server.config');
+
+// make static url to access scripts through html templates like 'src="./scripts/test.js"'
+server.use('/src', express.static(path.join(__dirname + '/src')));
 
 // load index.html on 127.0.0.1:8080/
 server.get( '/', (req, res) => {
     res.sendFile(path.join(__dirname + '/views/index.html'));
 } );
 
-// make static url to access scripts through html templates like 'src="./scripts/test.js"'
-server.use('/scripts', express.static(path.join(__dirname + '/scripts')));
-
-server.listen(port, () => {
-    console.log(`server is running on ${port} port!`);
+server.listen(config.port, () => {
+    console.log(`server is running on ${config.port} port!`);
 });
