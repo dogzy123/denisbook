c4de5e2ad0dd08e1140a21299e7027642ad7deb8
Artur Klesun <arturklesun@gmail.com>
2018-11-27 03:48:34 +0200

    Allow Github Pages in CORS

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ let server = http.createServer((req, res) => {
         res.setHeader('Content-Type', 'application/json');
 
         // Website you wish to allow to connect
-        res.setHeader('Access-Control-Allow-Origin', 'http://midiana.lv');
+        res.setHeader('Access-Control-Allow-Origin', '*');
 
         // Request methods you wish to allow
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
