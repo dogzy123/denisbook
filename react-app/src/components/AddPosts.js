aa3f8f86a8c833be4b909cd42362920a9be2c2a8
dogzy123 <dogzy123@mail.ru>
2018-11-29 01:32:54 +0200

    add shift+enter & validation

--- react-app/src/components/AddPosts.js
+++ react-app/src/components/AddPosts.js
@@ -8,11 +8,17 @@ class AddPosts extends Component {
         super(props);
 
         this.state = {
-            text : ''
+            text : '',
+            isError : false,
+            newLine : {
+                16 : false, // shift
+                13 : false  // enter
+            }
         };
 
         this.onKeyDown  = this.onKeyDown.bind(this);
         this.onInput    = this.onInput.bind(this);
+        this.onKeyUp    = this.onKeyUp.bind(this);
     }
 
 
@@ -33,23 +39,60 @@ class AddPosts extends Component {
 
     onInput (e) {
         this.setState({
-            text : e.target.value
+            ...this.state,
+            text : e.target.value,
+            isError : e.target.value.length < 1
         });
     }
 
-    onKeyDown (e) {
-        if (e.keyCode === 13)
+    onKeyUp (e) {
+        if (e.keyCode === 13 || e.keyCode === 16)
         {
-            e.target.blur();
-            e.target.value = "";
-            this.addPost();
+            this.setState( {
+                ...this.state,
+                newLine: {
+                    ...this.state.newLine,
+                    [e.keyCode] : false
+                }
+            } );
+        }
+    }
+
+    onKeyDown (e) {
+        if (e.keyCode === 16 || e.keyCode === 13)
+        {
+            this.setState( {
+                ...this.state,
+                newLine : {
+                    ...this.state.newLine,
+                    [e.keyCode] : true
+                }
+            } );
+
+            if (e.keyCode === 13 && !this.state.newLine['16'])
+            {
+                e.target.blur();
+
+                if (e.target.value.length < 1)
+                {
+                    e.target.value = "";
+                    return this.setState({
+                        ...this.state,
+                        isError : true
+                    })
+                }
+
+                e.target.value = "";
+                return this.addPost();
+            }
         }
     }
 
     render() {
+        console.log(this.state);
         return (
             <div className="create-post">
-                <textarea className="create-post-text" onKeyDown={this.onKeyDown} onInput={this.onInput} placeholder="Write something here..."></textarea>
+                <textarea className={"create-post-text" + (this.state.isError ? " error" : "") } onKeyDown={this.onKeyDown} onKeyUp={this.onKeyUp} onInput={this.onInput} placeholder="Write something here..."></textarea>
             </div>
         );
     }
